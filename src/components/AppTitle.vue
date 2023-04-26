5d8880ca84f7dac5d12ea752e81a4cb1cebbd18d
nico17022001 <nico17022001boolean@gmail.com>
2023-04-26 18:43:15 +0200

    bonus

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -10,9 +10,23 @@ export default {
 </script>
 
 <template>
-  <h1>{{ titolo }}</h1>
+  <div id="sfondo">
+    <h1>{{ titolo }}</h1>
+  </div>
 </template>
 
-<style>
+<style scoped>
+  h1{
+    color: white;
+  }
+  #sfondo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+    width: 100vw;
+    height: 100vh;
+  }
 
 </style>
