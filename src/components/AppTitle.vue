ad1b295a29b55c726bbd3777f12f96118f2af1ea
nico17022001 <nico17022001boolean@gmail.com>
2023-04-26 18:29:36 +0200

    fine-esercizio-vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "AppTitle",
+  data(){
+    return{
+      titolo: "La mia prima app con Vite!"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ titolo }}</h1>
+</template>
+
+<style>
+
+</style>
